7e8e7c195aa1675a08687680fe0f1ab725702324
o.rosario <o.rosario@alumnos.upm.es>
2024-09-13 19:34:28 +0200

    Nueva clase #1

---
+++ src/main/java/PaquetePruebas/Experiment.java
@@ -0,0 +1,13 @@
+package PaquetePruebas;
+
+public class Experiment {
+    int num;
+
+    public Experiment(int num){
+        this.num = num;
+    }
+
+    public int getNumber(){
+        return this.num;
+    }
+}
